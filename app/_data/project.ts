1d23514115332705cf268589854609c6113988bb
john lester escarlan <jlescarlan11@gmail.com>
2025-07-13 00:49:03 +0800

    modified dats

--- app/_data/project.ts
+++ app/_data/project.ts
@@ -5,19 +5,26 @@ export default async function getProjectData() {
     const response = await axios.get("/api/project");
     let projectData = [];
     if (response.data.success) {
-      projectData = response.data.data.filter((p: unknown): p is { status: string } => {
-        return typeof p === "object" && p !== null && "status" in p && (p as { status: string }).status === "featured";
-      });
+      projectData = response.data.data.filter(
+        (p: unknown): p is { status: string } => {
+          return (
+            typeof p === "object" &&
+            p !== null &&
+            "status" in p &&
+            (p as { status: string }).status === "featured"
+          );
+        }
+      );
     }
     return {
-      section: "Projects",
-      sectionDescription: "My personal projects and contributions.",
+      section: "My Projects",
+      sectionDescription: "Things I've built and worked on.",
       projectData,
     };
   } catch {
     return {
-      section: "Projects",
-      sectionDescription: "My personal projects and contributions.",
+      section: "My Projects",
+      sectionDescription: "Things I've built and worked on.",
       projectData: [],
     };
   }
